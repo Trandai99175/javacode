65c03feecfeb5124bd2561bc67623d574d5d7653
Trandai99175 <Trandai99175@gmail.com>
2022-04-21 00:21:59 +0700

    Java_bt1

---
+++ Day01/btvn1/untitled/src/Btvns1.java
@@ -0,0 +1,24 @@
+public class Btvns1 {
+    public static void main(String[] args) {
+        int a=10, b=20;
+        int x0= b-a;
+        System.out.println(x0);
+
+        int x1= a-b;
+        System.out.println(x1);
+
+        int x2= a+b;
+        System.out.println(x2);
+
+        float x3 = a/b;
+        System.out.println(x3);
+
+        float x4= b/a;
+        System.out.println(x4);
+
+        int x5= a*b;
+        System.out.println(x5);
+
+
+    }
+}
---
+++ Day01/btvn1/untitled/src/Method.java
@@ -0,0 +1,19 @@
+public class Method {
+    public static void main(String[] args) {
+        Method m = new Method();
+
+        m.sayHelloWithName("Cân nặng là 85kg");
+        m.sayHelloWithName("Chiều cao là 1.75m");
+        m.sayHelloWithName("Chỉ số BMI");
+        float data = m.bmi(85f, 1.75f);
+        System.out.println(data);
+    }
+
+    public void sayHelloWithName(String name) {
+        System.out.println("Thông tin " + name);
+    }
+
+    public float bmi(float a, float b) {
+        return a/b/b;
+    }
+}
